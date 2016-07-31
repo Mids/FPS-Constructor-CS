523ee769f1ab589d8c8113236600da3b4e087189
revolutehell <revolutehell@naver.com>
2016-07-31 19:38:43 +0900

    ReloadSounds - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/WeaponScripts/ReloadSounds.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class ReloadSounds : MonoBehaviour
+	{
+		public AudioClip sound1;
+		public AudioClip sound2;
+		public AudioClip sound3;
+		public AudioClip sound4;
+		public AudioClip sound5;
+		public AudioClip sound6;
+
+		public AudioSource audio;
+
+		public void Start()
+		{
+			if (audio == null)
+				audio = GetComponent<AudioSource>();
+			if (audio == null)
+				audio = gameObject.AddComponent<AudioSource>();
+		}
+
+		public void play1()
+		{
+			audio.clip = sound1;
+			audio.Play();
+		}
+
+		public void play2()
+		{
+			audio.clip = sound2;
+			audio.Play();
+		}
+
+		public void play3()
+		{
+			audio.clip = sound3;
+			audio.Play();
+		}
+
+		public void play4()
+		{
+			audio.clip = sound4;
+			audio.Play();
+		}
+
+		public void play5()
+		{
+			audio.clip = sound5;
+			audio.Play();
+		}
+
+		public void play6()
+		{
+			audio.clip = sound6;
+			audio.Play();
+		}
+	}
+}
