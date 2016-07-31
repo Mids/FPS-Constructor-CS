7019831c0353958b44ea2f3874a452150e23876c
revolutehell <revolutehell@naver.com>
2016-07-31 21:04:04 +0900

    HealthDisplay - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Editable/HealthDisplay.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+public class HealthDisplay : MonoBehaviour {
+
+	public PlayerHealth playerHealth;
+
+	void Start()
+	{
+		playerHealth = this.GetComponent<PlayerHealth>();
+	}
+
+	void OnGUI()
+	{
+		GUI.Box(new Rect(10, Screen.height - 30, 100, 20), "Health: " + Mathf.Round(playerHealth.health));
+	}
+}
+}
