02e1195b0f1a097139be6053475860837ce1dd6b
revolutehell <revolutehell@naver.com>
2016-07-31 19:07:48 +0900

    MuzzleFlash - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/WeaponScripts/MuzzleFlash.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class MuzzleFlash : MonoBehaviour
+	{
+		public bool isPrimary = true;
+
+		public void MuzzleFlash(bool temp)
+		{
+			BroadcastMessage("Activate", SendMessageOptions.DontRequireReceiver);
+			if (temp != isPrimary)
+				return;
+			var emitters = this.GetComponentsInChildren<ParticleEmitter>();
+			for (int i = 0; i < emitters.Length; i++)
+			{
+				ParticleEmitter p = emitters[i] as ParticleEmitter;
+				p.Emit();
+			}
+		}
+	}
+}
