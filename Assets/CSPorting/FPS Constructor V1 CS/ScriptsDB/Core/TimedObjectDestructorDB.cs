e9b5119100fa9191025bd577dadc28b8c9c1eeb7
revolutehell <revolutehell@naver.com>
2016-07-31 22:17:03 +0900

    TimedObjectDestructorDB - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Core/TimedObjectDestructorDB.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class TimedObjectDestructorDB : MonoBehaviour
+	{
+		public float timeOut = 1.0f;
+		public bool detachChildren = false;
+
+		void Awake()
+		{
+			Invoke("DestroyNow", timeOut);
+		}
+
+		public void DestroyNow()
+		{
+			if (detachChildren)
+			{
+				transform.DetachChildren();
+			}
+			DestroyObject(gameObject);
+		}
+	}
+}
