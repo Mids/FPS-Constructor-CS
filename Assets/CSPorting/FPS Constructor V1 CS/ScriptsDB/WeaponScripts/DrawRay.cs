f1f33458949380f900dbedcf04e88363e16165c5
revolutehell <revolutehell@naver.com>
2016-07-31 15:52:40 +0900

    DrawRay.cs

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/WeaponScripts/DrawRay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class DrawRay : MonoBehaviour
+	{
+		bool needsSelection = false;
+		bool negative = false;
+
+		void OnDrawGizmosSelected()
+		{
+			if (needsSelection)
+			{
+				Gizmos.color = Color.white;
+				Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 10));
+				Gizmos.DrawWireSphere(pos, 0.05f);
+			}
+		}
+
+		void OnDrawGizmos()
+		{
+			if (!needsSelection)
+			{
+				Gizmos.color = Color.white;
+				Vector3 pos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 10));
+				Gizmos.DrawWireSphere(pos, 0.05f);
+			}
+		}
+	}
+}
