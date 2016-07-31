831a5ba31b61d80026e7168f690b5614aec37724
revolutehell <revolutehell@naver.com>
2016-07-31 20:41:03 +0900

    Key - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Input/Key.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class Key : MonoBehaviour
+	{
+		public string key;
+		public InputItem input;
+
+		public void UpdateInput()
+		{
+			//Just get the values from Unity's input
+			input.got = Input.GetButton(key);
+			input.down = Input.GetButtonDown(key);
+			input.up = Input.GetButtonUp(key);
+		}
+	}
+}
