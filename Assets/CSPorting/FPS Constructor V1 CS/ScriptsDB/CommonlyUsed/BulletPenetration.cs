eccfbace1648c4cc227873d28dadead84aad0567
Jin Park <jiny199109@gmail.com>
2016-07-31 18:01:47 +0900

    BulletPenetration - initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/CommonlyUsed/BulletPenetration.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class BulletPenetration : MonoBehaviour 
+	{
+		public int penetrateValue = 1;
+	}
+}
