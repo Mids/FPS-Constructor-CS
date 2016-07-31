7e0966f2d11d7415606f8817398d406ca63e20ce
revolutehell <revolutehell@naver.com>
2016-07-31 21:04:10 +0900

    HighlightFX - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Editable/HighlightFX.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class HighlightFX : MonoBehaviour
+	{
+		private bool selected = false;
+		public string objectName;
+		public string description;
+
+		public void HighlightOn()
+		{
+			selected = true;
+		}
+
+		public void HighlightOff()
+		{
+			selected = false;
+		}
+
+		void OnGUI()
+		{
+			GUI.skin.box.wordWrap = true;
+			if (selected && !DBStoreController.inStore)
+			{
+				string s = "(Tab) to Use";
+
+				Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
+				GUI.Box(new Rect(pos.x - 77.5f, Screen.height - pos.y - (Screen.height / 4) - 52.5f, 155, 105), objectName + "\n \n" + description + "\n" + s);
+			}
+		}
+	}
+}
