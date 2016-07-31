8cc546c3227c3bfacabf3df3d99b1b3d37ba43d7
revolutehell <revolutehell@naver.com>
2016-07-31 21:04:35 +0900

    WeaponDisplay - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Editable/WeaponDisplay.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class WeaponDisplay : MonoBehaviour
+	{
+		private bool display = false;
+		private float endTime;
+		private GameObject weapon;
+		private WeaponInfo WeaponInfo;
+
+		public float displayTime = 2;
+
+		void Start()
+		{
+			WeaponInfo = GetComponent<WeaponInfo>();
+			display = false;
+		}
+		public void Select()
+		{
+			display = true;
+			endTime = Time.time + displayTime;
+		}
+
+		void OnGUI()
+		{
+			if (display && Time.time != 0.0f)
+			{
+				if (Time.time > endTime) display = false;
+				GUI.Box(new Rect(Screen.width - 255, Screen.height - 85, 135, 75), WeaponInfo.gunName + "\n" + WeaponInfo.gunDescription + "\n");
+			}
+		}
+	}
+}
