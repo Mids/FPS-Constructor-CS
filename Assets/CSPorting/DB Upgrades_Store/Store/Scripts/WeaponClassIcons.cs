acc03a15ff2b95a425e8d14f0a8ee2eb4a4f4c14
Jin Park <jiny199109@gmail.com>
2016-08-01 03:45:02 +0900

    WeaponClassIcons - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Store/Scripts/WeaponClassIcons.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class WeaponClassIcons : MonoBehaviour 
+	{
+		//Array to hold icons for the Weapon Classes
+		//A Custom Editor Script only displays the number of actual Weapon Classes Defined in WeaponInfo
+		public Texture[] weaponClassTextures = new Texture[20];
+	}
+}
