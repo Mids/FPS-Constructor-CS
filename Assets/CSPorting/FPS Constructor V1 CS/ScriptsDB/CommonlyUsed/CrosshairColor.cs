c4d82bb58180b897388133b6e3c9db6da8740513
Jin Park <jiny199109@gmail.com>
2016-07-31 18:04:31 +0900

    CrosshairColor - initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/CommonlyUsed/CrosshairColor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class CrosshairColor : MonoBehaviour 
+	{
+		//Custom editor 
+		public enum crosshairTypes { Friend, Foe, Other }
+		public crosshairTypes crosshairType;
+
+		private GameObject weaponCam;
+
+		public void Start()
+		{
+			weaponCam = GameObject.FindWithTag("WeaponCamera");
+		}
+	}
+}
