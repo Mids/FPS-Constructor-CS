4dad5abd7e76d88a556ac4677ad6989995a28d69
revolutehell <revolutehell@naver.com>
2016-07-31 20:40:21 +0900

    Axis - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Input/Axis.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class Axis : MonoBehaviour
+	{
+		public string key;
+		public InputItem input;
+
+		public void UpdateInput()
+		{
+			//Just get the axis value from Unity's input
+			input.axis = Input.GetAxisRaw(key);
+		}
+	}
+}
