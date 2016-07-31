3739f6a0b7359a2ebfca9ea754ca5c8629f0a554
revolutehell <revolutehell@naver.com>
2016-07-31 20:41:57 +0900

    WeaponSelector - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/Input/WeaponSelector.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class WeaponSelector : MonoBehaviour
+	{
+		public static int selectedWeapon; //this value must be changed to switch weapons.
+
+		void Awake()
+		{
+			selectedWeapon = 0;
+		}
+
+		void LateUpdate()
+		{
+			selectedWeapon = PlayerWeapons.PW.selectedWeapon;
+		}
+	}
+}
