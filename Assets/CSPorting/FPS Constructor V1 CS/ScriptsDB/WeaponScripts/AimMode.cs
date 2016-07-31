1bb385ffdcd80ecb04ae1ebeff4f9d99c7923750
revolutehell <revolutehell@naver.com>
2016-07-31 18:08:39 +0900

    AimMode - fix

--- Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/WeaponScripts/AimMode.cs
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/WeaponScripts/AimMode.cs
@@ -8,43 +8,43 @@ namespace ooparts.fpsctorcs
 	{
 		/////////////////////////// CHANGEABLE BY USER ///////////////////////////
 
-		Texture scopeTexture; //Currently used scope texture
-		bool overrideSprint = false;
-		int sprintDuration = 5; //how long can we sprint for?
-		float sprintAddStand = 1; //how quickly does sprint replenish when idle?
-		float sprintAddWalk = 0.3f; //how quickly does sprint replenish when moving?
-		float sprintMin = 1; //What is the minimum value ofsprint at which we can begin sprinting?
-		float recoverDelay = 0.7f; //how much time after sprinting does it take to start recovering sprint?
-		float exhaustedDelay = 1; //how much time after sprinting to exhaustion does it take to start recovering sprint?
-		bool crosshairWhenAiming = false;
+		public Texture scopeTexture; //Currently used scope texture
+		public bool overrideSprint = false;
+		public int sprintDuration = 5; //how long can we sprint for?
+		public float sprintAddStand = 1; //how quickly does sprint replenish when idle?
+		public float sprintAddWalk = 0.3f; //how quickly does sprint replenish when moving?
+		public float sprintMin = 1; //What is the minimum value ofsprint at which we can begin sprinting?
+		public float recoverDelay = 0.7f; //how much time after sprinting does it take to start recovering sprint?
+		public float exhaustedDelay = 1; //how much time after sprinting to exhaustion does it take to start recovering sprint?
+		public bool crosshairWhenAiming = false;
 
 		//Changes to the following variable will only be reflected when AimPrimary() or AimSecondary() are called
 		//Calling either while aiming is not suggested
 
 		//Zoom info for secondary weapon
-		float zoomFactor2 = 1;
-		bool scoped2 = false;
-		bool sightsZoom2 = false;
+		public float zoomFactor2 = 1;
+		public bool scoped2 = false;
+		public bool sightsZoom2 = false;
 		//Zoom info for primary weapon
-		float zoomFactor1 = 1;
-		bool scoped1 = false;
-		bool sightsZoom1 = false;
+		public float zoomFactor1 = 1;
+		public bool scoped1 = false;
+		public bool sightsZoom1 = false;
 
 		//Set of positions and rotations used for primary weapon
-		Vector3 aimPosition1;
-		Vector3 aimRotation1;
-		Vector3 hipPosition1;
-		Vector3 hipRotation1;
-		Vector3 sprintPosition1;
-		Vector3 sprintRotation1;
+		public Vector3 aimPosition1;
+		public Vector3 aimRotation1;
+		public Vector3 hipPosition1;
+		public Vector3 hipRotation1;
+		public Vector3 sprintPosition1;
+		public Vector3 sprintRotation1;
 
 		//Set of positions and rotations used for secondary weapon
-		Vector3 aimPosition2;
-		Vector3 aimRotation2;
-		Vector3 hipPosition2;
-		Vector3 hipRotation2;
-		Vector3 sprintPosition2;
-		Vector3 sprintRotation2;
+		public Vector3 aimPosition2;
+		public Vector3 aimRotation2;
+		public Vector3 hipPosition2;
+		public Vector3 hipRotation2;
+		public Vector3 sprintPosition2;
+		public Vector3 sprintRotation2;
 
 		private float aimStartTime;
 
@@ -58,10 +58,10 @@ namespace ooparts.fpsctorcs
 		*/
 
 
-		Texture st169; //scope texture for 16 : 9 aspect ration
-		Texture st1610; // 16 :10
-		Texture st43; // 4 : 3
-		Texture st54; // 5 : 4
+		public Texture st169; //scope texture for 16 : 9 aspect ration
+		public Texture st1610; // 16 :10
+		public Texture st43; // 4 : 3
+		public Texture st54; // 5 : 4
 		private GameObject player; //Player object
 		public bool scoped = false; //Does this weapon use a scope?
 		private float scopeTime; //Time when we should be in scope
@@ -78,40 +78,40 @@ namespace ooparts.fpsctorcs
 
 		private Vector3 deltaAngle;
 		private bool selected = false;
-		static float sprintNum;
-		float aimRate = 3;
-		float sprintRate = 0.4f;
-		float retRate = 0.4f;
+		public static float sprintNum;
+		public float aimRate = 3;
+		public float sprintRate = 0.4f;
+		public float retRate = 0.4f;
 		private GameObject cmra;
 		private GameObject wcmra;
 
-		float zoomFactor = 1; //how much does this zoom in when aiming? (currently)
+		public float zoomFactor = 1; //how much does this zoom in when aiming? (currently)
 
 		//Set of positions and rotations used
-		Vector3 aimPosition;
-		Vector3 aimRotation;
-		Vector3 hipPosition;
-		Vector3 hipRotation;
-		Vector3 sprintPosition;
-		Vector3 sprintRotation;
+		public Vector3 aimPosition;
+		public Vector3 aimRotation;
+		public Vector3 hipPosition;
+		public Vector3 hipRotation;
+		public Vector3 sprintPosition;
+		public Vector3 sprintRotation;
 
-		float rotationSpeed = 180;
-		CharacterController controller;
+		public float rotationSpeed = 180;
+		public CharacterController controller;
 		private bool zoomed = false;
 		public bool canSwitchWeaponAim = true;
-		static bool staticAiming = false;
-		bool hasSecondary = true;
-		GunScript GunScript1;
+		public static bool staticAiming = false;
+		public bool hasSecondary = true;
+		public GunScript GunScript1;
 		private Vector3 curVect;
 		private float sprintEndTime = 0;
 		private CharacterMotorDB CM;
-		static bool exhausted = false;
+		public static bool exhausted = false;
 		private bool switching = false;
 
 		private Vector3 startPosition;
 		private Vector3 startRotation;
 		private float moveProgress;
-		static float staticRate;
+		public static float staticRate;
 
 
 		void Start()
@@ -145,7 +145,7 @@ namespace ooparts.fpsctorcs
 			CM = GameObject.FindWithTag("Player").GetComponent<CharacterMotorDB>();
 		}
 
-		void AspectCheck()
+		public void AspectCheck()
 		{
 			if (cmra.GetComponent<Camera>().aspect == 1.6f && st1610 != null)
 			{
@@ -340,21 +340,21 @@ namespace ooparts.fpsctorcs
 			sprintingPublic = sprinting;
 		}
 
-		void DeselectWeapon()
+		public void DeselectWeapon()
 		{
 			selected = false;
 			inScope = false;
 			aiming = false;
 		}
 
-		void SelectWeapon()
+		public void SelectWeapon()
 		{
 			selected = true;
 			aiming = false;
 			SmartCrosshair.displayWhenAiming = crosshairWhenAiming;
 		}
 
-		void AimPrimary()
+		public void AimPrimary()
 		{
 			aimPosition = aimPosition1;
 			aimRotation = aimRotation1;
@@ -371,7 +371,7 @@ namespace ooparts.fpsctorcs
 			switching = true;
 		}
 
-		void AimSecondary()
+		public void AimSecondary()
 		{
 			aimPosition = aimPosition2;
 			aimRotation = aimRotation2;
@@ -388,7 +388,7 @@ namespace ooparts.fpsctorcs
 			switching = true;
 		}
 
-		void GetGunScript(int n)
+		public void GetGunScript(int n)
 		{
 			GunScript[] gunScripts = transform.parent.GetComponents<GunScript>();
 			foreach (GunScript gs in gunScripts)
@@ -404,7 +404,7 @@ namespace ooparts.fpsctorcs
 			}
 		}
 
-		void GunToRotation(Vector3 v3, float rate)
+		public void GunToRotation(Vector3 v3, float rate)
 		{
 			transform.localEulerAngles = new Vector3(Mathf.LerpAngle(startRotation.x, v3.x, Mathf.SmoothStep(0, 1, 1 - (aimStartTime - Time.time) / rate)), Mathf.LerpAngle(startRotation.y, v3.y, Mathf.SmoothStep(0, 1, 1 - (aimStartTime - Time.time) / rate)), Mathf.LerpAngle(startRotation.z, v3.z, Mathf.SmoothStep(0, 1, 1 - (aimStartTime - Time.time) / rate)));
 		}
