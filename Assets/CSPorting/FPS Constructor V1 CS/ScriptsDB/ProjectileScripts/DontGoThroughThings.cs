eb87a48bcf3fd703e2003146ba01a07b2cdc9146
revolutehell <revolutehell@naver.com>
2016-07-31 20:13:03 +0900

    DontGoThroughThings - commit

---
+++ Assets/CSPorting/FPS Constructor V1 CS/ScriptsDB/ProjectileScripts/DontGoThroughThings.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+namespace ooparts.fpsctorcs
+{
+	public class DontGoThroughThings : MonoBehaviour
+	{
+		public LayerMask layerMask = ~(1 << PlayerWeapons.playerLayer); //make sure we aren't in this layer 
+		public float skinWidth = 0.1f; //probably doesn't need to be changed 
+		private float minimumExtent;
+		private float partialExtent;
+		private float sqrMinimumExtent;
+		private Vector3 previousPosition;
+		private Rigidbody myRigidbody;
+		//initialize values 
+		void Awake()
+		{
+			myRigidbody = GetComponent<Rigidbody>();
+			previousPosition = myRigidbody.position;
+			minimumExtent = Mathf.Min(Mathf.Min(GetComponent<Collider>().bounds.extents.x, GetComponent<Collider>().bounds.extents.y), GetComponent<Collider>().bounds.extents.z);
+			partialExtent = minimumExtent * (1.0f - skinWidth);
+			sqrMinimumExtent = minimumExtent * minimumExtent;
+		}
+
+		void FixedUpdate()
+		{
+			//have we moved more than our minimum extent? 
+			Vector3 movementThisStep = myRigidbody.position - previousPosition;
+			float movementSqrMagnitude = movementThisStep.sqrMagnitude;
+			if (movementSqrMagnitude > sqrMinimumExtent)
+			{
+				float movementMagnitude = Mathf.Sqrt(movementSqrMagnitude);
+				RaycastHit hitInfo;
+				//check for obstructions we might have missed 
+				if (Physics.Raycast(previousPosition, movementThisStep, out hitInfo, movementMagnitude, layerMask.value))
+					myRigidbody.position = hitInfo.point - (movementThisStep / movementMagnitude) * partialExtent;
+			}
+			previousPosition = myRigidbody.position;
+		}
+	}
+}
