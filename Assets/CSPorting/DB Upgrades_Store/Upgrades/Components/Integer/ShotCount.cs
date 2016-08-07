f274046ec460149da6d2cab16dec5b882763fcb2
Jin Park <jiny199109@gmail.com>
2016-08-08 02:54:53 +0900

    Shot Count - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Integer/ShotCount.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ShotCount : MonoBehaviour 
+	{
+		public int val;
+		private int cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gscript)
+		{
+			cache = val - gscript.shotCount;
+			gscript.shotCount += cache;
+		}
+		public void Remove(GunScript gscript)
+		{
+			gscript.shotCount -= cache;
+		}
+	}
+}
