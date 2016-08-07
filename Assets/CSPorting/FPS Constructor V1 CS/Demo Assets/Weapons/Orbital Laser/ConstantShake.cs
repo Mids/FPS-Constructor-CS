21791fe0ca49eeb1472dcec2757cc7ce2e81b50b
Jin Park <jiny199109@gmail.com>
2016-08-07 23:39:40 +0900

    Constant Shake - initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Weapons/Orbital Laser/ConstantShake.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ConstantShake : MonoBehaviour 
+	{
+		public float amplitude;
+		public float time;
+
+		public void Update()
+		{
+			CameraShake.ShakeCam(amplitude, 10, time);
+		}
+	}
+}
