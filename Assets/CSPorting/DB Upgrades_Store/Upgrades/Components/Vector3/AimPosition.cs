6d5af76fbe24be125fdacd2dfcaee84402a594c0
Jin Park <jiny199109@gmail.com>
2016-08-08 02:37:30 +0900

    Aim Position - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Vector3/AimPosition.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class AimPosition : MonoBehaviour
+	{
+		[HideInInspector] public AimMode gscript;
+		public Vector3 val;
+		private Vector3 cache;
+		private bool applied = false;
+
+		public void Start()
+		{
+			gscript = this.transform.parent.GetComponent<GunScript>().GetComponentInChildren<AimMode>();
+		}
+
+		public void Apply()
+		{
+			cache = gscript.aimPosition1;
+			gscript.aimPosition1 = val;
+			gscript.aimPosition = val;
+		}
+
+		public void Remove()
+		{
+			gscript.aimPosition1 = cache;
+			gscript.aimPosition = cache;
+		}
+	}
+}
