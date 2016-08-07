1ac83bc330361b8a3bf92fd1f23cf82fd0dd353b
Jin Park <jiny199109@gmail.com>
2016-08-08 02:51:20 +0900

    AmmoSet - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Integer/AmmoSet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class AmmoSet : MonoBehaviour
+	{
+		public int val;
+		private int cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gscript)
+		{
+			gscript.ApplyToSharedAmmo();
+			cache = gscript.ammoSetUsed;
+			gscript.ammoSetUsed = val;
+			gscript.AlignToSharedAmmo();
+		}
+
+		public void Remove(GunScript gscript)
+		{
+			gscript.ApplyToSharedAmmo();
+			gscript.ammoSetUsed = cache;
+			gscript.AlignToSharedAmmo();
+		}
+	}
+}
