35271e9496692a6849c046b08a93af393500ddc4
Jin Park <jiny199109@gmail.com>
2016-08-07 23:27:45 +0900

    Laser - initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Weapons/Orbital Laser/Laser/Laser.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Laser : MonoBehaviour
+	{
+		float dps;
+		float power;
+		float forceRadius;
+		float vFactor;
+
+		void OnTriggerStay(Collider other)
+		{
+			Object[] sendArray = new Object[2];
+			sendArray[0] = (Object) (object) (dps * Time.deltaTime);
+			sendArray[1] = (Object) (object) true;
+			other.SendMessageUpwards("ApplyDamage", sendArray, SendMessageOptions.DontRequireReceiver);
+			if (other.GetComponent<Rigidbody>())
+				other.GetComponent<Rigidbody>().AddExplosionForce(power, transform.position, forceRadius, vFactor);
+		}
+
+		void Finish()
+		{
+			Destroy(transform.parent);
+		}
+
+		void ChargeLevel()
+		{
+			transform.parent.position = GameObject.FindWithTag("Laser").transform.position;
+		}
+	}
+}
