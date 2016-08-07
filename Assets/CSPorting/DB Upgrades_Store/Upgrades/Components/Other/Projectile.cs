eb09deefdbe04cb1bc63010fe884e338e724ddb1
Jin Park <jiny199109@gmail.com>
2016-08-08 02:47:11 +0900

    Projectile - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Other/Projectile.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Projectile : MonoBehaviour
+	{
+		public Rigidbody val;
+		private Rigidbody cache;
+		private bool applied = false;
+
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.projectile;
+			gScript.projectile = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.projectile = cache;
+		}
+	}
+}
