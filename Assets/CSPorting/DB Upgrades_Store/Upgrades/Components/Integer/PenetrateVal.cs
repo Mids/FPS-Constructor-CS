479f3108ca09d4cd1acc670070cce2db70ad7faf
Jin Park <jiny199109@gmail.com>
2016-08-08 02:54:13 +0900

    PenetrateVal - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Integer/PenetrateVal.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class PenetrateVal : MonoBehaviour
+	{
+		public int val;
+		private int cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gscript)
+		{
+			cache = val - gscript.penetrateVal;
+			gscript.penetrateVal += cache;
+		}
+
+		public void Remove(GunScript gscript)
+		{
+			gscript.penetrateVal -= cache;
+		}
+	}
+}
