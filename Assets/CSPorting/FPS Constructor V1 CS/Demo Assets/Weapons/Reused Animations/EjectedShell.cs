a9341bf446142c4cb4b5483d402d737f1d322745
Jin Park <jiny199109@gmail.com>
2016-08-07 23:53:27 +0900

    Ejected Shell - initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Weapons/Reused Animations/EjectedShell.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class EjectedShell : MonoBehaviour
+	{
+		public Vector3 force;
+		public float randomFactorForce;
+		//float gravity;
+		public Vector3 torque;
+		public float randomFactorTorque;
+
+		public void Start()
+		{
+			GetComponent<Rigidbody>().AddRelativeForce(force * Random.Range(1, randomFactorForce));
+			GetComponent<Rigidbody>().AddRelativeTorque(torque * Random.Range(-randomFactorTorque, randomFactorTorque));
+		}
+	}
+}
