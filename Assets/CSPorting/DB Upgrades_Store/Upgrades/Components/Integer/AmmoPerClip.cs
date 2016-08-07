fe97c3f38c68238d2ef1319e434f09329d918677
Jin Park <jiny199109@gmail.com>
2016-08-08 02:50:17 +0900

    Ammo Per Clip - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Integer/AmmoPerClip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class AmmoPerClip : MonoBehaviour
+	{
+		public int val;
+		private int cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gscript)
+		{
+			cache = val - gscript.ammoPerClip;
+			gscript.ammoPerClip += cache;
+			if (gscript.ammoLeft > gscript.ammoPerClip)
+				gscript.ammoLeft = gscript.ammoPerClip;
+		}
+
+		public void Remove(GunScript gscript)
+		{
+			gscript.ammoPerClip -= cache;
+			if (gscript.ammoLeft > gscript.ammoPerClip)
+				gscript.ammoLeft = gscript.ammoPerClip;
+		}
+
+		///when decreasing clip size add to clip reserve
+	}
+}
