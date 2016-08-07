7d36c30f4f07d7a6a6f46ab867fa0f3a960d6cb8
Jin Park <jiny199109@gmail.com>
2016-08-08 02:52:19 +0900

    BurstCount - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Integer/BurstCount.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class BurstCount : MonoBehaviour
+	{
+		public int val;
+		private int cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gscript)
+		{
+			cache = val - gscript.burstCount;
+			gscript.burstCount += cache;
+		}
+
+		public void Remove(GunScript gscript)
+		{
+			gscript.burstCount -= cache;
+		}
+	}
+}
