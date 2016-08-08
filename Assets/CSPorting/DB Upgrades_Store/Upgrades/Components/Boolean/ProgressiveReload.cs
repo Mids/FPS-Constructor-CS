c190c6c0b4c1c467eee882c03c0257c691f4e46f
Jin Park <jiny199109@gmail.com>
2016-08-09 02:57:00 +0900

    ProgressiveReload - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Boolean/ProgressiveReload.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ProgressiveReload : MonoBehaviour 
+	{
+		public bool val;
+		private bool cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.progressiveReload;
+			gScript.progressiveReload = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.progressiveReload = cache;
+		}
+	}
+}
