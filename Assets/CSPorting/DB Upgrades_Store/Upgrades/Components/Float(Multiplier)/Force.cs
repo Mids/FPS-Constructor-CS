5d6820f4d4d49569962fd7241c4bb1608440035a
Jin Park <jiny199109@gmail.com>
2016-08-09 02:41:11 +0900

    Force - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/Force.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Force : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.force * (multiplier - 1);
+			gScript.force += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.force -= cache;
+		}
+	}
+}
