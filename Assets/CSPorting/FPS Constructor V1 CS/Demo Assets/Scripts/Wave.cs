50cd46edae639d5109a9903809a2c3e78465759b
Jin Park <jiny199109@gmail.com>
2016-08-09 04:01:10 +0900

    Wave - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Scripts/Wave.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Wave : MonoBehaviour
+	{
+		public CubeSet[] cubeSets;
+	}
+}
