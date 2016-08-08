55d184ac02cc893adeefa578944dd9bc9d8981b7
Jin Park <jiny199109@gmail.com>
2016-08-09 04:35:40 +0900

    EnemyShoot - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Enemy/General Scripts/EnemyShoot.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class EnemyShoot : MonoBehaviour
+	{
+		private Transform target;
+		private float nextAttackTime;
+		public float damage;
+		public float force;
+		public float fireRate;
+		public Fire fire;
+		public GameObject tracer;
+		public Transform shootPos;
+		public float actualSpread;
+
+		public void Start()
+		{
+			target = PlayerWeapons.weaponCam.transform;
+		}
+
+		public void Attack()
+		{
+			if (Time.time < nextAttackTime)
+				return;
+			nextAttackTime = Time.time + fireRate;
+			//void  Fire ( int penetration ,   float damage ,   float force ,   GameObject tracer ,   Vector3 direction ,   Vector3 firePosition  ){
+			fire.Fire(0, damage, force, tracer, SprayDirection(), shootPos.position);
+		}
+
+		public Vector3 SprayDirection()
+		{
+			float vx = (1 - 2 * Random.value) * actualSpread;
+			float vy = (1 - 2 * Random.value) * actualSpread;
+			float vz = 1.0f;
+			return transform.TransformDirection(new Vector3(vx, vy, vz));
+		}
+	}
+}
