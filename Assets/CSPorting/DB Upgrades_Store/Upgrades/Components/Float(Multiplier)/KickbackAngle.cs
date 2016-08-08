14fce6d45cc68dbf286764d886d15323cfa1c006
Jin Park <jiny199109@gmail.com>
2016-08-09 02:41:23 +0900

    KickbackAngle - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/KickbackAngle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class KickbackAngle : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.kickbackAngle * (multiplier - 1);
+			gScript.kickbackAngle += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.kickbackAngle -= cache;
+		}
+	}
+}
