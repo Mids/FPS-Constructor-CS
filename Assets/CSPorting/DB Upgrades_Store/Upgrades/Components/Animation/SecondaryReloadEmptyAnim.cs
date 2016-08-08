be7a62bb3fea06016dc9c3d8e6b0bfabf50530fb
Jin Park <jiny199109@gmail.com>
2016-08-09 03:07:14 +0900

    SecondaryReloadEmptyAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/SecondaryReloadEmptyAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class SecondaryReloadEmptyAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().secondaryReloadEmpty;
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryReloadEmpty = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryReloadEmpty = cache;
+		}
+	}
+}
