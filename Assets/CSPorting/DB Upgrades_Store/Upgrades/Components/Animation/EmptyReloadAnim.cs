97297f3b42b2678e05123a20288567a0af6325e7
Jin Park <jiny199109@gmail.com>
2016-08-09 03:05:29 +0900

    EmptyReloadAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/EmptyReloadAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class EmptyReloadAnim : MonoBehaviour
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().emptyReloadAnim;
+			gScript.GetComponentInChildren<GunChildAnimation>().emptyReloadAnim = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().emptyReloadAnim = cache;
+		}
+	}
+}
