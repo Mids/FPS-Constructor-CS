3b6b2e4841b4ec9b9cd50cf6f0323691acb6b814
Jin Park <jiny199109@gmail.com>
2016-08-09 03:07:22 +0900

    WalkAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/WalkAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class WalkAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().walkAnimation;
+			gScript.GetComponentInChildren<GunChildAnimation>().walkAnimation = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().walkAnimation = cache;
+		}
+	}
+}
