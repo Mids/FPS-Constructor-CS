d9f9ff2b493a8bed4a5ab52f63d277594a02d64c
Jin Park <jiny199109@gmail.com>
2016-08-09 02:41:01 +0900

    FireRate - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/FireRate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class FireRate : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.fireRate * (multiplier - 1);
+			gScript.fireRate += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.fireRate -= cache;
+		}
+	}
+}
