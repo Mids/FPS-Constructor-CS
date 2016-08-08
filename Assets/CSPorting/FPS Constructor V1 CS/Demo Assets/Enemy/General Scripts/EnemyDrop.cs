f5cd43f9ef0f97cfae93ea451b26279e4c3288ec
Jin Park <jiny199109@gmail.com>
2016-08-09 04:35:16 +0900

    EnemyDrop - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Enemy/General Scripts/EnemyDrop.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class EnemyDrop : MonoBehaviour
+	{
+		Transform drops;
+		int min = 0;
+		int max = 5;
+		float force = 5;
+
+		void Die()
+		{
+			int amt = Random.Range(min, max);
+			int i = 0;
+			Transform t;
+			Vector3 dir;
+			while (i < amt)
+			{
+				t = ((GameObject) Instantiate(drops, transform.position + new Vector3(0, 2, 0), transform.rotation)).transform;
+
+				dir = Random.insideUnitSphere * force;
+				t.GetComponent<Rigidbody>().AddForce(dir, ForceMode.Impulse);
+				t.GetComponent<Rigidbody>().AddTorque(dir, ForceMode.Impulse);
+				i++;
+			}
+		}
+	}
+}
