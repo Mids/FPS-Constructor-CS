4e1a0c31fe310811e7af352954bb6fbb1580d3bb
Jin Park <jiny199109@gmail.com>
2016-08-09 02:43:28 +0900

    Volume - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/Volume.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Volume : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.fireVolume * (multiplier - 1);
+			gScript.fireVolume += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.fireVolume -= cache;
+		}
+	}
+}
