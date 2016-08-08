91bb12447123c5997466e0d210fb11efb1b831d3
Jin Park <jiny199109@gmail.com>
2016-08-09 02:57:10 +0900

    Scoped - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Boolean/Scoped.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Scoped : MonoBehaviour
+	{
+		private AimMode gscript;
+		public bool val;
+		private bool cache;
+		private bool applied = false;
+
+		public void Start()
+		{
+			gscript = this.transform.parent.GetComponent<GunScript>().GetComponentInChildren<AimMode>();
+		}
+
+		public void Apply()
+		{
+			cache = gscript.scoped1;
+			gscript.scoped = val;
+			gscript.scoped1 = val;
+		}
+
+		public void Remove()
+		{
+			gscript.scoped = cache;
+			gscript.scoped1 = cache;
+		}
+	}
+}
