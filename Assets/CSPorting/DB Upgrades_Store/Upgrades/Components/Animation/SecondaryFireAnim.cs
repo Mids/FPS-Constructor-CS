d92a7987425ebbb9d3507b9159f51d8dc244f14c
Jin Park <jiny199109@gmail.com>
2016-08-09 03:06:45 +0900

    SecondaryFireAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/SecondaryFireAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class SecondaryFireAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().secondaryFireAnim;
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryFireAnim = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryFireAnim = cache;
+		}
+	}
+}
