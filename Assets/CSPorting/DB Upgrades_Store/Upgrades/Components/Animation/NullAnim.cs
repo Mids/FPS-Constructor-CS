93fb91de53148601b9a551524cd713f4cc26e9c0
Jin Park <jiny199109@gmail.com>
2016-08-09 03:05:46 +0900

    NullAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/NullAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class NullAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().nullAnim;
+			gScript.GetComponentInChildren<GunChildAnimation>().nullAnim = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().nullAnim = cache;
+		}
+	}
+}
