c9d17410cd81e3b7e0dff8b36aeac4e840958b49
Jin Park <jiny199109@gmail.com>
2016-08-09 03:05:38 +0900

    FireAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/FireAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class FireAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().fireAnim;
+			gScript.GetComponentInChildren<GunChildAnimation>().fireAnim = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().fireAnim = cache;
+		}
+	}
+}
