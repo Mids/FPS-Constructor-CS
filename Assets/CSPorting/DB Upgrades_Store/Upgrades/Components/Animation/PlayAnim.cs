cee7913259afb517c7f05e2a73031d27a91a63b0
Jin Park <jiny199109@gmail.com>
2016-08-09 03:06:01 +0900

    PlayAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/PlayAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class PlayAnim : MonoBehaviour
+	{
+		public string removeAnim;
+		public string applyAnim;
+
+		public void Apply(GunScript g)
+		{
+			if (applyAnim != "" && g.gunActive)
+				transform.parent.BroadcastMessage("PlayAnim", applyAnim);
+		}
+
+		public void Remove(GunScript g)
+		{
+			if (removeAnim != "" && g.gunActive)
+				transform.parent.BroadcastMessage("PlayAnim", applyAnim);
+		}
+	}
+}
