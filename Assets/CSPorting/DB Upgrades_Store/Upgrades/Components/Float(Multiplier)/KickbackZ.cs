0f532a12577e893c0f9da41c68015a28b22d2f06
Jin Park <jiny199109@gmail.com>
2016-08-09 02:41:42 +0900

    KickbackZ - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/KickbackZ.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class KickbackZ : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.kickBackZ * (multiplier - 1);
+			gScript.kickBackZ += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.kickBackZ -= cache;
+		}
+	}
+}
