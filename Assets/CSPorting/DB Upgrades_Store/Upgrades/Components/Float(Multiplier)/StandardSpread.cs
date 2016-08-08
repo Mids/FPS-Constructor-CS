0b25e338ca4d8c74a24ff362cb768d0ad62fff84
Jin Park <jiny199109@gmail.com>
2016-08-09 02:43:07 +0900

    StandardSpread - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/StandardSpread.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class StandardSpread : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.standardSpread * (multiplier - 1);
+			gScript.standardSpread += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.standardSpread -= cache;
+		}
+	}
+}
