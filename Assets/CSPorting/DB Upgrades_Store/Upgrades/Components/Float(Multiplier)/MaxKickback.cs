530740637115c9484511eba27caf38807c328556
Jin Park <jiny199109@gmail.com>
2016-08-09 02:41:54 +0900

    MaxKickback - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/MaxKickback.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class MaxKickback : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.maxKickback * (multiplier - 1);
+			gScript.maxKickback += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.maxKickback -= cache;
+		}
+	}
+}
