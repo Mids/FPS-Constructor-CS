3b8e0810625ad1e563d83ad06b2a918a7d72aac8
Jin Park <jiny199109@gmail.com>
2016-08-09 02:42:03 +0900

    MaxKickbackZ - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/MaxKickbackZ.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class MaxKickbackZ : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.maxZ * (multiplier - 1);
+			gScript.maxZ += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.maxZ -= cache;
+		}
+	}
+}
