6e8281894f85f5dcc659e56692521faf14eed0d8
Jin Park <jiny199109@gmail.com>
2016-08-09 03:53:36 +0900

    CubeSet - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Scripts/CubeSet.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class CubeSet : MonoBehaviour
+	{
+		public Transform[] cubes;
+		public int[] amts;
+
+		public IEnumerator SpawnCS(Transform pos, Waypoint w, float t)
+		{
+			Transform spawned;
+			for (int j = 0; j < cubes.Length; j++)
+			{
+				for (int q = 0; q < amts[j]; q++)
+				{
+					spawned = (Transform) Instantiate(cubes[j], pos.position + new Vector3(0, 4, 0) * q, pos.rotation);
+					EnemyMovement.enemies++;
+					spawned.GetComponent<EnemyMovement>().waypoint = w.transform;
+					yield return new WaitForSeconds(t);
+				}
+			}
+		}
+	}
+}
