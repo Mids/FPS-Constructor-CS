8d6989a513b3efac72905ee8f88580beb81f225b
Jin Park <jiny199109@gmail.com>
2016-08-09 02:25:10 +0900

    AimSpread - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/AimSpread.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class AimSpread : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.aimSpread * (multiplier - 1);
+			gScript.aimSpread += cache;
+		}
+		public void Remove(GunScript gScript)
+		{
+			gScript.aimSpread -= cache;
+		}
+	}
+}
