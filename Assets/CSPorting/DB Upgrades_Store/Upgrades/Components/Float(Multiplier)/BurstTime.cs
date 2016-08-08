61f57f36213b7f62b1fe3116f1efb66c166dfda3
Jin Park <jiny199109@gmail.com>
2016-08-09 02:27:40 +0900

    BurstTime - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/BurstTime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class BurstTime : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.burstTime * (multiplier - 1);
+			gScript.burstTime += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.burstTime -= cache;
+			cache = 0;
+		}
+	}
+}
