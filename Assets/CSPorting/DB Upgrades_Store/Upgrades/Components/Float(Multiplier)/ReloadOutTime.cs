87bc5365e423792ac6634da0a083b1d43ebb23ba
Jin Park <jiny199109@gmail.com>
2016-08-09 02:42:34 +0900

    ReloadOutTime - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/ReloadOutTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ReloadOutTime : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.reloadOutTime * (multiplier - 1);
+			gScript.reloadOutTime += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.reloadOutTime -= cache;
+		}
+	}
+}
