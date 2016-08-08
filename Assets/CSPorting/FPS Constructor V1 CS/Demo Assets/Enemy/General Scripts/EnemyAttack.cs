5422578ee143ca1626f78adf3dbefdf538eead69
Jin Park <jiny199109@gmail.com>
2016-08-09 04:35:06 +0900

    EnemyAttack - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Enemy/General Scripts/EnemyAttack.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class EnemyAttack : MonoBehaviour
+	{
+		private Transform target;
+		private float nextAttackTime;
+		public float damage;
+		public float attackTime;
+
+		public void Start()
+		{
+			target = PlayerWeapons.weaponCam.transform;
+		}
+
+		public void Attack()
+		{
+			if (Time.time < nextAttackTime)
+				return;
+			Object[] sendArray = new Object[2];
+			sendArray[0] = (Object) (object) damage;
+			sendArray[1] = (Object) (object) false;
+			target.SendMessageUpwards("ApplyDamage", sendArray, SendMessageOptions.DontRequireReceiver);
+			target.SendMessageUpwards("Direction", transform, SendMessageOptions.DontRequireReceiver);
+			nextAttackTime = Time.time + attackTime;
+			GetComponent<Animation>().Play();
+		}
+	}
+}
