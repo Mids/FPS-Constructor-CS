3a21293c4147a2d1df97fb7da156f5034df440e6
Jin Park <jiny199109@gmail.com>
2016-08-09 02:56:40 +0900

    BurstFire - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Boolean/BurstFire.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class BurstFire : MonoBehaviour 
+	{
+		public bool val;
+		private bool cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.burstFire;
+			gScript.burstFire = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.burstFire = cache;
+		}
+	}
+}
