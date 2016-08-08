c07b4a903ff7194a28c0e1f0072f07d4b90e8bee
Jin Park <jiny199109@gmail.com>
2016-08-09 03:06:55 +0900

    SecondaryNullAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/SecondaryNullAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class SecondaryNullAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().secondaryNullAnim;
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryNullAnim = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().secondaryNullAnim = cache;
+		}
+	}
+}
