70c4cc9c4aa310a15ef0ad82ce662ac692bbe2c4
Jin Park <jiny199109@gmail.com>
2016-08-09 03:06:26 +0900

    ReloadInAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/ReloadInAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ReloadInAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().reloadIn;
+			gScript.GetComponentInChildren<GunChildAnimation>().reloadIn = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().reloadIn = cache;
+		}
+	}
+}
