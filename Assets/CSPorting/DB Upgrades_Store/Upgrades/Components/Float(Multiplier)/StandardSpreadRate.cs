d225d5c5034b0b5297f027ae044b07eb60dbc5b4
Jin Park <jiny199109@gmail.com>
2016-08-09 02:43:17 +0900

    StandardSpreadRate - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/StandardSpreadRate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class StandardSpreadRate : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.standardSpreadRate * (multiplier - 1);
+			gScript.standardSpreadRate += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.standardSpreadRate -= cache;
+		}
+	}
+}
