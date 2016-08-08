bc0b9acce9c78e3d43515b7ed6a024a9417240de
Jin Park <jiny199109@gmail.com>
2016-08-09 02:43:34 +0900

    Zoom - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/Zoom.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Zoom : MonoBehaviour
+	{
+		private AimMode gscript;
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Start()
+		{
+			gscript = this.transform.parent.GetComponent<GunScript>().GetComponentInChildren<AimMode>();
+		}
+
+		public void Apply()
+		{
+			cache = gscript.zoomFactor1 * (multiplier - 1);
+			gscript.zoomFactor1 += cache;
+			gscript.zoomFactor = gscript.zoomFactor1;
+		}
+
+		public void Remove()
+		{
+			gscript.zoomFactor1 -= cache;
+			gscript.zoomFactor = gscript.zoomFactor1;
+		}
+	}
+}
