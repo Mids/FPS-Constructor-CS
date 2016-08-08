9de8c0cfbc2fcd191417b8df967393eab5eadd7c
Jin Park <jiny199109@gmail.com>
2016-08-09 02:26:04 +0900

    Aim Spread Rate - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/AimSpreadRate.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class AimSpreadRate : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.aimSpreadRate * (multiplier - 1);
+			gScript.aimSpreadRate += cache;
+		}
+		public void Remove(GunScript gScript)
+		{
+			gScript.aimSpreadRate -= cache;
+		}
+	}
+}
