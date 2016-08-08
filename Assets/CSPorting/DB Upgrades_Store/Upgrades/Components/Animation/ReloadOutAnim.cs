f217928831b95d7c1d822b91e985c3ad9e9d1cc1
Jin Park <jiny199109@gmail.com>
2016-08-09 03:06:35 +0900

    ReloadOutAnim - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Animation/ReloadOutAnim.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ReloadOutAnim : MonoBehaviour 
+	{
+		public string val;
+		private string cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.GetComponentInChildren<GunChildAnimation>().reloadOut;
+			gScript.GetComponentInChildren<GunChildAnimation>().reloadOut = val;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.GetComponentInChildren<GunChildAnimation>().reloadOut = cache;
+		}
+	}
+}
