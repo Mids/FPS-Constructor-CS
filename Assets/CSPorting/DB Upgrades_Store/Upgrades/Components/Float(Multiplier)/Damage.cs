4e2b588b95ba2ff8ea7062f1ede20812372b3460
Jin Park <jiny199109@gmail.com>
2016-08-09 02:29:31 +0900

    Damage - initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/Damage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class Damage : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.damage * (multiplier - 1);
+			gScript.damage += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.damage -= cache;
+		}
+	}
+}
