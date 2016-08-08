3649111279edece98c2e597d7f6eee9775d5db66
Jin Park <jiny199109@gmail.com>
2016-08-09 02:42:26 +0900

    ReloadInTime - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/ReloadInTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class ReloadInTime : MonoBehaviour
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.reloadInTime * (multiplier - 1);
+			gScript.reloadInTime += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.reloadInTime -= cache;
+		}
+	}
+}
