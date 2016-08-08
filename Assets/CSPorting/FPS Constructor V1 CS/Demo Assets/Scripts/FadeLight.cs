a92162c6f4e1f16568498d66a53acbcc4287a3ce
Jin Park <jiny199109@gmail.com>
2016-08-09 03:55:17 +0900

    FadeLight - Initial

---
+++ Assets/CSPorting/FPS Constructor V1 CS/Demo Assets/Scripts/FadeLight.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class FadeLight : MonoBehaviour
+	{
+		public float delay;
+		public float fadeTime;
+
+		private float fadeSpeed;
+		private float intensity;
+		private Color color;
+
+		public void Start()
+		{
+			if (GetComponent<Light>() == null)
+			{
+				Destroy(this);
+				return;
+			}
+
+			intensity = GetComponent<Light>().intensity;
+
+
+			fadeTime = Mathf.Abs(fadeTime);
+
+			if (fadeTime > 0.0f)
+			{
+				fadeSpeed = intensity / fadeTime;
+			}
+			else
+			{
+				fadeSpeed = intensity;
+			}
+			//alpha = 1.0f;
+		}
+
+		public void Update()
+		{
+			if (delay > 0.0f)
+			{
+				delay -= Time.deltaTime;
+			}
+			else if (intensity > 0.0f)
+			{
+				intensity -= fadeSpeed * Time.deltaTime;
+				GetComponent<Light>().intensity = intensity;
+			}
+		}
+	}
+}
