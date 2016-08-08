d8ade5b337ac0ef245b6a13c74b2a47a1ecbf68e
Jin Park <jiny199109@gmail.com>
2016-08-09 02:42:57 +0900

    SpreadDecreaseRate - Initial

---
+++ Assets/CSPorting/DB Upgrades_Store/Upgrades/Components/Float(Multiplier)/SpreadDecreaseRate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using ooparts.fpsctorcs;
+
+namespace ooparts.fpsctorcs
+{
+	public class SpreadDecreaseRate : MonoBehaviour 
+	{
+		public float multiplier = 1.5f;
+		private float cache;
+		private bool applied = false;
+
+		public void Apply(GunScript gScript)
+		{
+			cache = gScript.spDecRate * (multiplier - 1);
+			gScript.spDecRate += cache;
+		}
+
+		public void Remove(GunScript gScript)
+		{
+			gScript.spDecRate -= cache;
+		}
+	}
+}
