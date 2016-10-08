a42c479982343ead5779e6f535a4faab0cdbdced
Jungjin Park <Jungjin Park>
2016-10-08 20:27:35 +0900

    maxClip - fix

--- Assets/RPG/Abilities/MaxClip.cs
+++ Assets/RPG/Abilities/MaxClip.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System;
+using ooparts.fpsctorcs;
 
 public class MaxClip : Ability
 {
+	GunScript weapon = PlayerWeapons.PW.weapons[PlayerWeapons.PW.selectedWeapon].GetComponent<GunScript>(); //currently equipped weapon	
+
 	public override void Enhance()
 	{
-		throw new NotImplementedException();
+		weapon.maxClips++;
 	}
 }
